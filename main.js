f605dcf068f8b6422073e66f64326d0d7dadf73d
AllanLimitless <allanlimitless55@gmail.com>
2020-06-07 10:56:13 -0400

    Added words array

--- main.js
+++ main.js
@@ -7,4 +7,50 @@ $( document ).ready(function() {
   const scoreDisplay = document.querySelector('#score');
   const timeDisplay = document.querySelector('#time');
   const message = document.querySelector('#message');
+  
+  // Words 
+  const words = [
+    'algorithm',
+    'program',
+    'api',
+    'division',
+    'argument',
+    'boolean',
+    'bug',
+    'orbitz',
+    'char',
+    'class',
+    'addition',
+    'conditionals',
+    'javascript',
+    'constants',
+    'array',
+    'declaration',
+    'exception',
+    'expression',
+    'framework',
+    'loop',
+    'iteration',
+    'keywords',
+    'null',
+    'operand',
+    'variable',
+    'pointer',
+    'package',
+    'runtime',
+    'syntax',
+    'token',
+    'arity',
+    'anonymous',
+    'closure',
+    'currying',
+    'hoisting',
+    'mutation',
+    'sentinal',
+    'vanilla',
+    'pragma',
+    'variadic'
+  ]; 
+   
+   
 });
