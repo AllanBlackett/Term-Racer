c7d401ffb09955f243fdff3cb6af14826e16df0e
AllanLimitless <allanlimitless55@gmail.com>
2020-06-07 09:58:03 -0400

    Added DOM elements

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 // $( document ).ready() block.
 $( document ).ready(function() {
 
+   // DOM Elements
+  const wordInput = document.querySelector('#placeholder');
+  const currentWord = document.querySelector('#current-word');
+  const scoreDisplay = document.querySelector('#score');
+  const timeDisplay = document.querySelector('#time');
+  const message = document.querySelector('#message');
 });
